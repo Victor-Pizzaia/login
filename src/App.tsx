0387acc33829c50919ff464f73da1e901d37c9da
vpizzaia <victorhugo.pizzaia@dxc.com>
2021-12-16 17:18:04 -0300

    Creating login home and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "./styles/loginStyles.scss";
+import googleIcon from "./assets/GoogleIcon.svg";
+import AsideImage from "./assets/PeopleLogin.svg";
+
+type Login = {
+  email: string;
+  password: string;
+  remember: boolean;
+};
 
 function App(): JSX.Element {
+  const [{ email, password, remember }, setState] = React.useState<Login>({
+    email: "",
+    password: "",
+    remember: false,
+  });
+
+  const changeState = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setState(prev => ({ ...prev, [name]: value }));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="login-form">
+      <aside>
+        <img src={AsideImage} alt="Imagem do login" />
+      </aside>
+      <main>
+        <div className="form">
+          <h2>Bem vindo de volta</h2>
+          <h1>Faça login na sua conta</h1>
+          <span>E-mail</span>
+          <input
+            type="text"
+            name="email"
+            className="textField"
+            value={email}
+            onChange={changeState}
+          />
+          <span>Senha</span>
+          <input
+            type="password"
+            name="password"
+            className="textField"
+            value={password}
+            onChange={changeState}
+          />
+          <div className="rememberMe">
+            <div>
+              <input
+                type="radio"
+                name="remember"
+                checked={remember}
+                onChange={e =>
+                  setState(prev => ({ ...prev, remember: e.target.checked }))
+                }
+              />
+              <span>Lembre de mim </span>
+            </div>
+            <a href="">Esqueceu sua senha? </a>
+          </div>
+          <button>Entrar</button>
+          <button className="googleAuth">
+            <img src={googleIcon} alt="Logo do Google" />
+            Ou faça login com o Google
+          </button>
+        </div>
+        <div>
+          <span>
+            Não tem uma conta? <a href="">Cadastre-se</a>
+          </span>
+        </div>
+      </main>
     </div>
   );
 }
